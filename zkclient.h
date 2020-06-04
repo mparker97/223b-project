7957b5e9430815bd5eaff219fd4f7d5dee0511ea
htoo97 <htoo97@gmail.com>
2020-06-04 02:41:00 -0700

    Update zkclient header file

--- zkclient.h
+++ zkclient.h
@@ -29,6 +29,13 @@ void watcher(zhandle_t *zzh, int type, int state, const char *path, void* contex
 int zk_release_interval_lock(zk_lock_context_t *context);
 int zk_acquire_interval_lock(zk_lock_context_t *context);
 static int _zk_interval_lock_operation(zk_lock_context_t *context, struct timespec *ts);
+static int _zk_determine_interval_lock_eligibility(zk_lock_context_t *context, struct timespec *ts);
+static int _get_sorted_shifted_relevant_intervals(zk_lock_context_t* context, it_array_t* ret_array);
+static it_node_t** _sort_interval_locks_by_offset_id(struct String_vector * interval_children);
+static void _free_intervals_array(it_node_t** intervals_array, int len);
 static char* getLockName(char* str);
+static size_t getSequenceNumber(char* lock_name);
+int cmpSequenceFunc(const void * a, const void * b);
+int cmpOffsetIdFunc(const void * a, const void * b);
 
 #endif
